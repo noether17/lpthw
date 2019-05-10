825bfed33ec88d60b5af853eb9fabbd4767bf99b
Jon White <jawhite@oakland.edu>
2019-05-10 18:20:13 -0400

    through ex6

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Jon A. White'
+my_age = 26
+my_height = 73
+my_weight = 220
+my_eyes = 'Hazil'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually, that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are {my_teeth}.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
